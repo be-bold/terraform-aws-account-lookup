9bb0591704b21167672ef1114f84b691efdbb584
Jannes Heinrich <jannes.heinrich@be-bold.today>
2024-01-08 15:18:01 +0100

    Removed named provider alias

--- data.tf
+++ data.tf
@@ -1,9 +1,6 @@
-data "aws_organizations_organization" "this" {
-  provider = aws.org_management_account
-}
+data "aws_organizations_organization" "this" {}
 
 data "aws_organizations_resource_tags" "this" {
   for_each = toset(local.all_account_ids)
-  provider = aws.org_management_account
   resource_id = each.key
 }
