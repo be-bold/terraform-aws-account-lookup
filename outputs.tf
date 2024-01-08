b0b5d780d36611e426dff4384ae22bcbbe8062e7
Jannes Heinrich <jannes.heinrich@be-bold.today>
2024-01-08 16:21:14 +0100

    Refactored code

--- outputs.tf
+++ outputs.tf
@@ -8,66 +8,39 @@ output "organization_id" {
 # Management account
 output "management_account_id" {
   value = local.management_account_id
-  description = "The AWS account id of the organization's management account."
+  description = "The AWS account id of the organizations management account."
 }
 
 output "management_account_name" {
   value = local.management_account_name
-  description = "The name of the organization's management account."
+  description = "The name of the organizations management account."
 }
 
 
-# Lists of account ids
-output "organization_members_account_ids" {
-  value = local.member_account_ids
-  description = "A list of AWS account ids of all member accounts. It doesn't include the organization's management account."
+# Mappings
+output "mapping_id_to_name" {
+  value = local.mapping_id_to_name
+  description = "A map which contains the id of the accounts as key and the corresponding account name as value"
 }
 
-output "all_account_ids" {
-  value = local.all_account_ids
-  description = "A list of all AWS account ids of the organization. This includes both management account as well as all member accounts."
+output "mapping_name_to_id" {
+  value = local.mapping_name_to_id
+  description = "A map which contains the name of the accounts as key and the corresponding account id as value"
 }
 
-
-# Mapping between account id and name
-output "organization_members_mapping_name_to_account_id" {
-  value = local.organization_members_mapping_name_to_account_id
-  description = "A map which contains the name of the accounts as key and the corresponding account id as value for all member accounts of an organization. It doesn't include the organization's management account."
+output "mapping_id_to_tags" {
+  value = local.mapping_id_to_tags
+  description = "A map containing the account id as key and the corresponding tags of the account as value"
 }
 
-output "organization_members_mapping_account_id_to_name" {
-  value = local.organization_members_mapping_account_id_to_name
-  description = "A map which contains the id of the accounts as key and the corresponding account name as value for all member accounts of an organization. It doesn't include the organization's management account."
-}
-
-output "all_accounts_mapping_name_to_account_id" {
-  value = local.all_accounts_mapping_name_to_account_id
-  description = "A map which contains the name of the accounts as key and the corresponding account id as value for all accounts of the organization including the management account."
-}
-
-output "all_accounts_mapping_account_id_to_name" {
-  value = local.all_accounts_mapping_account_id_to_name
-  description = "A map which contains the id of the accounts as key and the corresponding account name as value for all accounts of the organization including the management account."
+output "mapping_name_to_tags" {
+  value = local.mapping_name_to_tags
+  description = "A map containing the account name as key and the corresponding tags of the account as value"
 }
 
 
-# Tagging
-output "all_accounts_tags_by_account_id" {
-  value = local.all_accounts_tags_by_account_id
-  description = "A map containing the account id as key and the corresponding tags of the account as value for all accounts including the management account."
-}
-
-output "all_accounts_tags_by_account_name" {
-  value = local.all_accounts_tags_by_account_name
-  description = "A map containing the account name as key and the corresponding tags of the account as value for all accounts including the management account."
-}
-
-output "organization_members_account_tags_by_account_id" {
-  value = local.organization_members_account_tags_by_account_id
-  description = "A map containing the account id as key and the corresponding tags of the account as value. Contains member accounts only."
-}
-
-output "organization_members_account_tags_by_account_name" {
-  value = local.organization_members_account_tags_by_account_name
-  description = "A map containing the account name as key and the corresponding tags of the account as value. Contains member accounts only."
+# Account list
+output "account_list" {
+  value = local.account_list
+  description = "A list of the accounts and all their properties."
 }
