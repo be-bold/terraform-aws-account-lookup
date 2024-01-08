389c9059b49e10e598e9dc4bd93ef5e18ed53b39
Jannes Heinrich <jannes.heinrich@be-bold.today>
2024-01-08 15:18:01 +0100

    Removed named provider alias

--- provider.tf
+++ provider.tf
@@ -1,11 +1,9 @@
 terraform {
   required_version = ">=1.3.0"
+
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      configuration_aliases = [
-        aws.org_management_account,
-      ]
+      source = "hashicorp/aws"
     }
   }
 }
