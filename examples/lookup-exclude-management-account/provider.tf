3bdf86dad663eb6523dca90652a3e873eb33e2bb
Jannes Heinrich <jannes.heinrich@be-bold.today>
2024-01-09 16:29:05 +0100

    Adjusted examples to not contains specific versions

--- examples/lookup-exclude-management-account/provider.tf
+++ examples/lookup-exclude-management-account/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.31.0"
+      version = "#.#.#"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "####"
   alias  = "organization_read_role"
 
   assume_role {
